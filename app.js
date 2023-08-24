f6e8abf17deb0ddc02e30767c9399c6a22da4be6
Deepak Kushwaha <95577575+mr-alhn@users.noreply.github.com>
2023-08-24 15:35:46 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
-const http = require('http');
-const mserver = require('./server');
+const http = require("http");
+const mserver = require("./server");
 
 const port = process.env.PORT || 3000;
-
 const server = http.createServer(mserver);
 
-server.listen(port);
+
+
+server.listen(port, () => {
+  console.log(`Server is running on port http://localhost:${port}`);
+});
